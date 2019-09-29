a387a116d18a43537420116c1b0a319c02e29519
emiliesedziol <sedziole@gmail.com>
2019-09-29 12:37:18 -0400

    testing commit

--- youtubeProject1/Program.cs
+++ youtubeProject1/Program.cs
@@ -6,9 +6,28 @@ namespace youtubeProject1
     {
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("Insert another Line");
-            Console.WriteLine("Hello World!");
+            string  Dog1 = "Cassian";
+            int DogAge1 = 1;
+            string HowFarWalks = "5 mile";
+            Console.WriteLine("");
+            Console.WriteLine("    /|\\");
+            Console.WriteLine("   /_|_\\");
+            Console.WriteLine("  /__|__\\");
+            Console.WriteLine(" /___|___\\");
+            Console.WriteLine("/____|____\\");
+            Console.WriteLine("");
+            Console.WriteLine("Dogs in the Neighborhood");
+            Console.WriteLine(Dog1 + "  " + DogAge1);
+            Console.WriteLine(Dog1 + " walks " + HowFarWalks + " a day");
+            Console.WriteLine("Brewen");
+            Console.WriteLine("Embry");
+            Dog1 = "Nala";
+            DogAge1 = 0;
+            HowFarWalks = ".1 mile";
+            Console.WriteLine(Dog1 + "  " + DogAge1);
+            Console.WriteLine(Dog1 + " walks " + HowFarWalks + " a day");
+            Console.WriteLine("Jeffery");
+            Console.ReadLine();
         }
     }
 }
