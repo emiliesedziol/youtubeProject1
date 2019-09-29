0f1a9d4efa345a9363aa9f0fe0df3dfa52859805
emiliesedziol <sedziole@gmail.com>
2019-09-29 11:54:01 -0400

    Add project files.

---
+++ youtubeProject1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace youtubeProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Insert another Line");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
