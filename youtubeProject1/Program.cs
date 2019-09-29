6b03310a6834826f68d7b05ca9bfb554b62f743a
emiliesedziol <sedziole@gmail.com>
2019-09-29 12:48:14 -0400

    data types

--- youtubeProject1/Program.cs
+++ youtubeProject1/Program.cs
@@ -6,7 +6,7 @@ namespace youtubeProject1
     {
         static void Main(string[] args)
         {
-            string  Dog1 = "Cassian";
+            string Dog1 = "Cassian";
             int DogAge1 = 1;
             string HowFarWalks = "5 mile";
             Console.WriteLine("");
@@ -27,6 +27,12 @@ namespace youtubeProject1
             Console.WriteLine(Dog1 + "  " + DogAge1);
             Console.WriteLine(Dog1 + " walks " + HowFarWalks + " a day");
             Console.WriteLine("Jeffery");
+            // data type
+            string  Dog2 = "Cassian";
+            // char age = '';  // only 1 characters
+           //  int DogAge = 1; //  no decimals can be used
+            double DogAgeMonths = .7;  // float, double, decimal
+            Console.WriteLine("Who live at my house "+ Dog2 + " and is " + DogAgeMonths + " old");
             Console.ReadLine();
         }
     }
