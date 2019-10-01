440c3196361e8e31f988773eec6d02a3a4bb8eed
emiliesedziol <sedziole@gmail.com>
2019-10-01 11:09:09 -0400

    git

--- youtubeProject1/Program.cs
+++ youtubeProject1/Program.cs
@@ -4,6 +4,8 @@ namespace youtubeProject1
 {
     class Program
     {
+        private const string Value = "A number wasn't entered";
+
         static void Main(string[] args)
         {
             string Dog1 = "Cassian";
@@ -78,10 +80,62 @@ namespace youtubeProject1
             String newName = Console.ReadLine();
             Console.WriteLine("Dogs name " + newName);
 
+
+            int num = Convert.ToInt32("45");
+
+            /*
+            Console.Write("Enter a number ");
+            string tmp1 = Console.ReadLine();
+            int num2 = 0;
+            if (int.TryParse(tmp1, out num2))
+            {
+                Console.WriteLine("number " + num2 + " was entered");
+            }
+            else
+            {
+                Console.WriteLine("A number wasn't entered " + num2 + " will be used");
+            }
+
+            
+            Console.ReadLine();
+            */
+
+            // calculator
+            Console.Write("Enter a Number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter a Number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+            /* interesting output
+             *  Enter a Number:3.4
+                Enter a Number:2.3
+                5.699999999999999
+            */
+
             Console.ReadLine();
 
+            // mad lib
 
-            // why isn't this going to git
+            Console.WriteLine("Roses are Red");
+            Console.WriteLine("Violets are Blue");
+            Console.WriteLine("Who loves you");
+
+            string color, pluralNoun, celebrity;
+            Console.Write("Enter a color ");
+            color = Console.ReadLine();
+            Console.Write("Enter something ");
+            pluralNoun = Console.ReadLine();
+            Console.Write("Enter a name");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are Blue");
+            Console.WriteLine("Who loves " + celebrity);
+
+            Console.ReadLine();
         }
     }
 }
