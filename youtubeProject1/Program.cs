4b7a8cef75c9e37c7c9170b80f4ccdcd14f86b59
emiliesedziol <sedziole@gmail.com>
2019-10-08 13:33:38 -0400

    ll

--- youtubeProject1/Program.cs
+++ youtubeProject1/Program.cs
@@ -4,53 +4,24 @@ namespace youtubeProject1
 {
     class Program
     {
-        private const string Value = "A number wasn't entered";
+ 
 
         static void Main(string[] args)
         {
 
-            String retrunDay = GetDay(4);
+            int index = 1;
+            while(index < 10)
+            {
+                Console.WriteLine("Index " + index);
+                index++;
+            }
 
-            Console.WriteLine(retrunDay);
 
             Console.ReadLine();
 
         }
 
-        static string GetDay(int dayNum)
-        {
-            string dayName;
-
-            switch (dayNum)
-            {
-                case 0:
-                    dayName = "Sunday";
-                    break;
-                case 1:
-                    dayName = "Monday";
-                    break;
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-                case 3:
-                    dayName = "Wednsday";
-                    break;
-                case 4:
-                    dayName = "Thursday";
-                    break;
-                case 5:
-                    dayName = "Friday";
-                    break;
-                case 6:
-                    dayName = "Saturday";
-                    break;
-                default:
-                    dayName = "Invalid Day";
-                    break;
-            }
-
-            return dayName;
-        }
+       
 
     }
 }
---
+++ youtubeProject1/Program2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace youtubeProject2
+{
+    class Program2
+    {
+        private const string Value = "A number wasn't entered";
+
+        static void Main2(string[] args)
+        {
+
+            String retrunDay = GetDay(4);
+
+            Console.WriteLine(retrunDay);
+
+            Console.ReadLine();
+
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednsday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day";
+                    break;
+            }
+
+            return dayName;
+        }
+
+    }
+}
