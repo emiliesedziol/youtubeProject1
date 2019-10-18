483b34623bfb58030732e34bfa411b53f565c3c8
emiliesedziol <sedziole@gmail.com>
2019-10-18 09:46:26 -0400

    l

--- youtubeProject1/Program.cs
+++ youtubeProject1/Program.cs
@@ -16,6 +16,19 @@ namespace youtubeProject1
 
             Console.ReadLine();
 
+            int[,] numberGrid =
+            {
+                {1,2 },
+                {3,4 },
+                {5,6 }
+
+            };
+            Console.WriteLine(numberGrid[1, 1]);
+
+            int[,] myArray = new int[2, 3];  // have to define the size of the array
+
+            Console.ReadLine();
+
         }
 
         static int GetPow( int baseNum, int powNum)
