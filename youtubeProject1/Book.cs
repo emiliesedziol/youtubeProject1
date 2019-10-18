9d7382cd9e9b7e2ca663c0a436e1f5eb4d5a8566
emiliesedziol <sedziole@gmail.com>
2019-10-18 13:33:15 -0400

    s

--- youtubeProject1/Book.cs
+++ youtubeProject1/Book.cs
@@ -9,5 +9,14 @@ namespace youtubeProject1
         public string title;
         public string author;
         public int pages;
+
+        // constructor for the class
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            Console.WriteLine("Creating book");  // every time an instance of book is created this line will print
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
     }
 }
--- youtubeProject1/Program.cs
+++ youtubeProject1/Program.cs
@@ -10,14 +10,24 @@ namespace youtubeProject1
         {
             // data type - Class model represents world entities - specification
 
+            Book book1 = new Book("Thrawn", "unknown", 500);
+            Book book2 = new Book("other", "unknown", 400);
+            /*
             // book object includes title, author and pages - instance of a book
-            Book book1 = new Book();
+            Book book1 = new Book();  // this calls the constructor method in Book class
             book1.title = "Thrawn";
             book1.author = "unknown";
             book1.pages = 1000;
 
             Console.WriteLine(book1.title);
 
+            Book book2 = new Book();  // this calls the constructor method in Book class
+            book2.title = "big boy";
+            book2.author = "unknown";
+            book2.pages = 100;
+            */
+            Console.WriteLine(book2.title);
+            
 
             Console.ReadLine();
 
